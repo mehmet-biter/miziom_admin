7c4759834263e3a8d5cc7d7c5c4d29f518241701
fil303 <webfil303@gmail.com>
2024-01-10 18:04:37 +0600

    update common api

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,9 @@ class Cors
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return $next($request);
+        //     ->header('Access-Control-Allow-Origin', '*')
+        //     ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        //     ->header('Access-Control-Allow-Credentials', 'false');
     }
 }
