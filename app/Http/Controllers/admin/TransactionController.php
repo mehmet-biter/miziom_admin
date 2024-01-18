6d295ae791d1984c948558e70a7b5d3853031070
fil303 <webfil303@gmail.com>
2024-01-18 17:22:31 +0600

    update

--- app/Http/Controllers/admin/TransactionController.php
+++ app/Http/Controllers/admin/TransactionController.php
@@ -59,9 +59,9 @@ class TransactionController extends Controller
                     return api_settings($wdrl->network_type);
                 })
                 ->addColumn('actions', function ($wdrl) {
-                    $action = '<div class="activity-icon">';
-                    $action .= '<a href="'.route("withdrawalApproveProccess",['id' => $wdrl->id]).'" class="btn btn-secondary btn-sm">Approve</a>';
-                    $action .= '<a href="'.route("withdrawalRejecteProccess",['id' => $wdrl->id]).'" class="btn btn-danger btn-sm">Reject</a>';
+                    $action = '<div class="activity-icon" style="display: flex; align-items: center; gap:4px;">';
+                    $action .= '<a title="'.__('Approve').'" href="'.route("withdrawalApproveProccess",['id' => $wdrl->id]).'"><svg style="fill:gray;" xmlns="http://www.w3.org/2000/svg" height="18" width="18" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg></a>';
+                    $action .= '<a title="'.__('Reject').'" href="'.route("withdrawalRejecteProccess",['id' => $wdrl->id]).'" ><svg style="fill:gray;" xmlns="http://www.w3.org/2000/svg" height="18" width="18" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg></a>';
                     $action .= '</div>';
 
                     return $action;
