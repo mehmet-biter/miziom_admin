5b4fdf7e4ba38497f55f55a82e64f1d5bf7acd0a
“Tajuddin <tajnwu@gmail.com>
2024-01-18 16:58:13 +0600

    Faq Update

--- resources/views/custom_pages/pages.blade.php
+++ resources/views/custom_pages/pages.blade.php
@@ -33,9 +33,9 @@
                             @if(isset($pages))
                             @foreach($pages as $p)
                                 <tr>
-                                    <td> {{$p->title}} </td>
-                                    <td> {{$p->slug}} </td>
-                                    <td>
+                                    <td class="!px-[18px]"> {{$p->title}} </td>
+                                    <td class="!px-[18px]"> {{$p->slug}} </td>
+                                    <td class="!px-[18px]">
                                         <div>
                                             <label class="w-12 h-6 relative">
                                                 <input onclick="return processForm('{{$p->id}}')" type="checkbox"
@@ -46,18 +46,22 @@
                                             </label>
                                         </div>
                                     </td>
-                                    <td> {{$p->updated_at}}</td>
-                                    <td> {{$p->created_at}}</td>
-                                    <td>
-                                        <ul class="d-flex activity-menu flex">
+                                    <td class="!px-[18px]"> {{$p->updated_at}}</td>
+                                    <td class="!px-[18px]"> {{$p->created_at}}</td>
+                                    <td class="!px-[18px]">
+                                        <ul class="flex gap-4 items-center">
                                             <li class="viewuser">
-                                                <a href="{{route('adminCustomPageEdit', ($p->id))}}" title="{{__("Update")}}" class="btn btn-primary btn-sm w-1">
-                                                    <i class="fa fa-pencil"></i>
+                                                <a href="{{route('adminCustomPageEdit', ($p->id))}}" title="{{__("Update")}}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512" class="fill-gray-400">
+                                                        <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/>
+                                                    </svg>
                                                 </a>
                                             </li>
                                             <li class="viewuser">
-                                                <a href="#" onclick="showAlert('{{ ($p->id) }}')" title="{{__("Delete")}}" class="btn btn-danger btn-sm">
-                                                    <i class="fa fa-regular fa-square-caret-up"></i>
+                                                <a href="#" onclick="showAlert('{{ ($p->id) }}')" title="{{__("Delete")}}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512" class="fill-gray-400">
+                                                        <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                                                    </svg>
                                                 </a>
                                             </li>
                                         </ul>
