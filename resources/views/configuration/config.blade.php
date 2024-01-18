c6531296c26106992aa0795ae4d7f66b25d94973
“Tajuddin <tajnwu@gmail.com>
2024-01-18 15:36:51 +0600

    Update

--- resources/views/configuration/config.blade.php
+++ resources/views/configuration/config.blade.php
@@ -1,6 +1,6 @@
 <x-layout.default>
     <div>
-        <div class="panel px-0 border-[#e0e6ed] dark:border-[#1b2e4b]">
+        <div class="panel px-5 border-[#e0e6ed] dark:border-[#1b2e4b]">
             <div class="px-5">
                 <div class=" md:top-5 ltr:md:left-5 rtl:md:right-5">
                     <div class="flex items-center gap-2 mb-5">
@@ -9,9 +9,9 @@
                 </div>
             </div>
 
-            <div class="flex flex-wrap m-auto">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
-                <div class="m-3 max-w-[22rem] w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
+                <div class="w-full px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
                     <div class="py-7 px-6">
                         <h5 class="text-[#3b3f5c] text-[15px] font-bold mb-4 dark:text-white-light">{{ __("Clear Application Cache") }}</h5>
                         <p class="text-white-dark ">{{ __('From here you can clear your application all configuration . or also from the command line you can run the command "php artisan cache:clear"') }}</p>
@@ -20,7 +20,7 @@
                     </div>
                 </div>
 
-                <div class="m-3 max-w-[22rem] w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
+                <div class="w-full px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
                     <div class="py-7 px-6">
                         <h5 class="text-[#3b3f5c] text-[15px] font-bold mb-4 dark:text-white-light">{{ __("Clear Application Config") }}</h5>
                         <p class="text-white-dark ">{{ __('From here you can clear your application cache . or also from the command line you can run the command "php artisan config:clear"') }}</p>
@@ -29,7 +29,7 @@
                     </div>
                 </div>
 
-                <div class="m-3 max-w-[22rem] w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
+                <div class="w-full px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
                     <div class="py-7 px-6">
                         <h5 class="text-[#3b3f5c] text-[15px] font-bold mb-4 dark:text-white-light">{{ __("Clear Application View / Route") }}</h5>
                         <p class="text-white-dark ">{{ __('From here you can clear your application view and route . or also from the command line you can run the command "php artisan view:clear", "php artisan route:clear"') }}</p>
@@ -38,7 +38,7 @@
                     </div>
                 </div>
 
-                <div class="m-3 max-w-[22rem] w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
+                <div class="w-full px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
                     <div class="py-7 px-6">
                         <h5 class="text-[#3b3f5c] text-[15px] font-bold mb-4 dark:text-white-light">{{ __("Run Migration") }}</h5>
                         <p class="text-white-dark ">{{ __('For the new migration you can click the button to migrate or run the command "php artisan migrate"') }}</p>
@@ -47,7 +47,7 @@
                     </div>
                 </div>
 
-                <div class="m-3 max-w-[22rem] w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
+                <div class="w-full px-2 mb-4 bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
                     <div class="py-7 px-6">
                         <h5 class="text-[#3b3f5c] text-[15px] font-bold mb-4 dark:text-white-light">{{ __("Run Schedule") }}</h5>
                         <p class="text-white-dark ">{{ __('In this command we use some command, that should always run in the background') }}</p>
